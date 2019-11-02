c8f196ec8b9d614e9fb97e55d1a078f4b974a87e
Juan <juana.guevara@urosario.edu.co>
2019-11-02 15:51:46 -0500

    'Juego'

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -1,96 +1,36 @@
-#include <iostream>
+#include "tree.hpp"
 #include <cstdlib>
-using namespace std;
 
-bool turnoDeX = false;
+bool turnoDeX = false; //Jugador "O" marca primero
 
 char tablero[][3] = {{'_', '_', '_'},
-                     {'_', '_', '_'},
+                     {'_', '_', '_'}, //Tablero vacío
                      {'_', '_', '_'},
 };
 
-class Tree {
-private:
-  struct {
-    char situacion[][3] = {{'_', '_', '_'},
-                           {'_', '_', '_'},
-                           {'_', '_', '_'},
-    };
-    Node* parent;
-    Node* one;
-    Node* two;
-    Node* three;
-    Node* four;
-    Node* five;
-    Node* six;
-    Node* seven;
-    Node* eight;
-    Node* nine;
-  };
-  Node* root;
-  void destroyRecursive(Node* p);
-  void Crear_arbol(Node* &p, Node* &ptr, char element);
-public:
-  Tree();
-  ~Tree();
-};
-
-Tree::Tree() {
-  root = nullptr;
-}
-
-Tree::destroyRecursive(Node* p) {
-  if (p != nullptr) {
-    destroyRecursive(p->one);
-    destroyRecursive(p->two);
-    destroyRecursive(p->three);
-    destroyRecursive(p->four);
-    destroyRecursive(p->five);
-    destroyRecursive(p->six);
-    destroyRecursive(p->seven);
-    destroyRecursive(p->eight);
-    destroyRecursive(p->nine);
-    delete p;
-  }
-}
-
-Tree::~Tree() {
-  destroyRecursive(root);
-}
-
-Tree::Crear_arbol(Node* &p, Node* &ptr, char element) {
-  if (ptr == nullptr) {
-    for (int i = 0; i < 3; i++) {
-      for (int j = 0; j < 3; j++) {
-          if (ptr->situacion[i][j] == "_") {
-            Node* nNode = new Node;
-            nNode -> situacion[i][j] = element;
-          }
-      }
-    }
-  }
-}
-
+//Retorna "X" o "O"
 char hayGanador() {
-    //validacion horizontal
+    //Validacion horizontal
     for (int i = 0; i < 3; i++) {
         if (tablero[i][0] != '_' && tablero[i][0] == tablero[i][1] && tablero[i][1] == tablero[i][2]) {
             return tablero[i][0];
         }
     }
-    //validacion vertical
+    //Validacion vertical
     for (int i = 0; i < 3; i++) {
         if (tablero[0][i] != '_' && tablero[0][i] == tablero[1][i] && tablero[1][i] == tablero[2][i]) {
             return tablero[0][i];
         }
     }
-    //validacion diagonal izquierda a derecha
+    //Validacion diagonal izquierda a derecha
     if (tablero[0][0] != '_' && tablero[0][0] == tablero[1][1] && tablero[1][1] == tablero[2][2])
         return tablero[0][0];
 
-    //validacion diagonal derecha a izquierda
+    //Validacion diagonal derecha a izquierda
     if (tablero[0][2] != '_' && tablero[0][2] == tablero[1][1] && tablero[1][1] == tablero[2][0])
         return tablero[0][2];
+	
+	return '_';
 }
 
 bool hayEmpate() {
@@ -114,6 +54,7 @@ void mostrarTablero() {
         }
         cout << endl;
     }
+	cout << endl;
     cout << "Turno de " << jugador << endl;
     cout << "En que fila desea jugar: ";
     cin >> fila;
@@ -121,6 +62,7 @@ void mostrarTablero() {
     cout << "En que columna desea jugar: ";
     cin >> columna;
     columna--;
+	cout << endl;
     tablero[fila][columna] = jugador;
     char ganador = hayGanador();
     if (ganador != '_') {
@@ -135,8 +77,10 @@ void mostrarTablero() {
 }
 
 int main() {
-    while(true) {
+    /*while(true) {
         mostrarTablero();
-    }
+    }*/
+	Tree t;
+	t.display();
     return 0;
 }
